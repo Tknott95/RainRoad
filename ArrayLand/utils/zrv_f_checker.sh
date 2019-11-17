746452be4c098c512ba1da513af326acace7d3b3
Trevor Knott <tknott95@hotmail.com>
2019-11-17 13:52:15 -0700

    ArchitRefactor quickCommit

--- ArrayLand/utils/zrv_f_checker.sh
+++ ArrayLand/utils/zrv_f_checker.sh
@@ -1,6 +1,6 @@
 RUNTIME="./runtime/$1"
 if test -f $RUNTIME; then
-  g++ ./src/$1.cpp -o ./runtime/$1 && ./runtime/$1
+  g++ ./src/$1/$1.cpp -o ./runtime/$1 && ./runtime/$1
 else
-  g++ ./src/$1.cpp -o ./runtime/$1
+  g++ ./src/$1/$1.cpp -o ./runtime/$1
 fi
