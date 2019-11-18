ed6da3002076e6a4dd1df1d0d3013a56ab177dff
Trevor Knott <tknott95@hotmail.com>
2019-11-17 20:09:58 -0700

    ArrayBS quickCommit

--- ArrayLand/src/02/02.cpp
+++ ArrayLand/src/02/02.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 
 int main() {
-  std::cout << "02" << std::endl;
+  const int byteSize = 11;
+  int test[byteSize];
   
+  test[0] = 4;
+  test[1] = 44;
+  test[2] = 444;
+  test[3] = 33;
+  test[4] = 40;
+  test[5] = 1101;
+  test[6] = 1110;
+  test[7] = 0101;  
+
+  for(int i = 0; i < byteSize; i++) {
+    std::cout << "\n" << test[i] << std::endl;
+  }
   return 0;
 }
