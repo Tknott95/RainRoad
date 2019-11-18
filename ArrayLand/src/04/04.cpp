69bb64d42efdd940156c415fdff44027fab56b84
Trevor Knott <tknott95@hotmail.com>
2019-11-17 21:23:45 -0700

    Prime # Generator

--- ArrayLand/src/04/04.cpp
+++ ArrayLand/src/04/04.cpp
@@ -9,9 +9,9 @@ int main() {
 
   for(int i = 1; i < byteSize; i++) {
     const double powerTwo = pow(2, (i - 1));
-      if(fmod(powerTwo, i) == 1) {
-        cout << "Prime: " << i << endl;
-      }
+    if(fmod(powerTwo, i) == 1) {
+      cout << "Prime: " << i << endl;
+    }
   }
 
   return 0;
