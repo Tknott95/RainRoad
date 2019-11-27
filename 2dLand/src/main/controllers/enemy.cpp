d67856faa4c8f46c5c47e60f8a25a98012801cd8
Trevor Knott <tknott95@hotmail.com>
2019-11-26 17:20:55 -0700

    EnemyClass quickCommit

--- 2dLand/src/main/controllers/enemy.cpp
+++ 2dLand/src/main/controllers/enemy.cpp
@@ -1,5 +1,5 @@
 #include "../headers/enemy.h"
-/* REFACTOR THIS REFACTOR THIS */
+
 void Enemy::init() {
   this->_enemy.setPosition(50.f, 50.f);
   this->_enemy.setSize(sf::Vector2f(133, 133));
@@ -7,4 +7,3 @@ void Enemy::init() {
   this->_enemy.setOutlineColor(sf::Color::White);
   this->_enemy.setOutlineThickness(1.8f);
 }
-/* REFACTOR THIS REFACTOR THIS */
