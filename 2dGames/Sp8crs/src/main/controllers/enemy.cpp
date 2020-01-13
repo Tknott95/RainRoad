5a7b20cd8ce48d9d1a34b45adbdb3225d20311ce
Trevor Knott <Tknott95@hotmail.com>
2020-01-12 19:38:44 -0700

    Enemy miniTwks | sp8crs

--- 2dGames/Sp8crs/src/main/controllers/enemy.cpp
+++ 2dGames/Sp8crs/src/main/controllers/enemy.cpp
@@ -24,12 +24,12 @@ void Enemy::spawn(sf::Vector2f pos, EnemyType eType) {
       cout << "ERROR: Could not load enemy texture file." << "\n";
     }
     e00.enemy.setTexture(this->_texture00);
-    e00.enemy.scale(1.0f, 1.0f);
+    e00.enemy.scale(1.3f, 1.28f);
   } else if(eType == sheriff) {
     if(!this->_texture01.loadFromFile("utils/img/enemy/e2.png")) {
         cout << "ERROR: Could not load enemy texture file." << "\n";
     }
-    e00.enemy.scale(0.6f, 0.8f);
+    e00.enemy.scale(0.58f, 0.74f);
     e00.enemy.setTexture(this->_texture01);     
     const sf::Vector2f _orgn = {50, 0};
     e00.enemy.setOrigin(_orgn);
@@ -52,11 +52,11 @@ void Enemy::spawner(int currLvl) {
     this->spawn(sf::Vector2f(555.f, 300.f), sheriff); 
     /* @TODO create lookAtPlayer() then a case/if statement to choose which to run regarding type */
   } else if (currLvl == 2) {
-    this->spawn(sf::Vector2f(600.f, 200.f), sheriff);
+    this->spawn(sf::Vector2f(80.f, 200.f), sheriff);
     this->spawn(sf::Vector2f(1000.f, 200.f), kamikaze);
     this->spawn(sf::Vector2f(333.f, 100.f), kamikaze);
     this->spawn(sf::Vector2f(100.f, 000.f), kamikaze);
-    this->spawn(sf::Vector2f(555.f, 170.f), sheriff);
+    this->spawn(sf::Vector2f(755.f, 170.f), sheriff);
     this->spawn(sf::Vector2f(220.f, 150.f), kamikaze);
   } else if (currLvl == 3) { /* @TODO create new types of enemies w/ the enum made a few days prior */
     this->spawn(sf::Vector2f(600.f, 200.f), sheriff);
