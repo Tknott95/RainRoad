2eb743866ad44aa2b8a31d8e1cdaafe3b6a5119f
Trevor Knott <Tknott95@hotmail.com>
2020-01-20 23:58:26 -0700

    Misc

--- 2dGames/Sp8crs/src/main/controllers/enemy.cpp
+++ 2dGames/Sp8crs/src/main/controllers/enemy.cpp
@@ -50,12 +50,11 @@ void Enemy::spawn(sf::Vector2f pos, EnemyType eType) {
 
 void Enemy::spawner(int currLvl) {
   if(currLvl == 1) {
-    this->spawn(sf::Vector2f(600.f, 200.f), sixer); /* was kamikaze */
+    this->spawn(sf::Vector2f(600.f, 200.f), sixer);
     this->spawn(sf::Vector2f(1000.f, 200.f), kamikaze);
     // this->spawn(sf::Vector2f(333.f, 100.f), kamikaze);
     this->spawn(sf::Vector2f(100.f, 000.f), kamikaze);
     this->spawn(sf::Vector2f(555.f, 300.f), sheriff); 
-    /* @TODO create lookAtPlayer() then a case/if statement to choose which to run regarding type */
   } else if (currLvl == 2) {
     this->spawn(sf::Vector2f(80.f, 200.f), sheriff);
     this->spawn(sf::Vector2f(1000.f, 200.f), kamikaze);
