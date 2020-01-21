dc35561a2fdb20990fbaf3c8df21948fc2e304da
Trevor Knott <Tknott95@hotmail.com>
2020-01-20 23:56:46 -0700

    Misc

--- 2dGames/Sp8crs/zrv.sh
+++ 2dGames/Sp8crs/zrv.sh
@@ -1,5 +1,5 @@
 P1="./src/$1"
 P1C="$P1/controllers"
 P1CU="$P1C/utils"
-# make list and loop through
+# @TODO  make list and loop through
 g++ $P1/$1.cpp $P1C/player.cpp $P1C/enemy.cpp $P1C/bullet.cpp $P1CU/overlay.cpp -o ./runtime/$1 -lsfml-audio -lsfml-graphics -lsfml-window -lsfml-system && ./runtime/$1
