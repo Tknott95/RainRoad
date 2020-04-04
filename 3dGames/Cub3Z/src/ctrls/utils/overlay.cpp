d45c22ae1f8763e3eb3cb0add98f5cc309801ae0
Trevor Knott <Tknott95@hotmail.com>
2020-04-04 14:11:45 -0600

    Cub3Zz_Hotfix

--- 3dGames/Cub3Z/src/ctrls/utils/overlay.cpp
+++ 3dGames/Cub3Z/src/ctrls/utils/overlay.cpp
@@ -11,20 +11,17 @@ void Overlay::Init(float screenWidth, float screenHeight) {
   this->_text00.setOutlineColor(sf::Color::Black);
   this->_text00.setOutlineThickness(2.f);
   this->_text00.setPosition(20, 10);
-  // this->_text00.setPosition((screenWidth/2)*0.35, screenHeight/2 - 200);
   this->_text00.setFillColor(sf::Color(40, 140, 240, 110));
 
   this->_text01.setFont(this->_font00);
   this->_text01.setCharacterSize(12);
   this->_text01.setPosition(20, screenHeight-50);
-  // this->_text01.setPosition((screenWidth/2)*0.45, screenHeight/2);
   this->_text01.setFillColor(sf::Color(40, 140, 240, 10));
   
 }
 
 Overlay::Overlay() {
   this->_clock00.restart();
-  // this->_timeElapsed00 = this->_clock00.getElapsedTime();
 }
 
 Overlay::~Overlay() {
