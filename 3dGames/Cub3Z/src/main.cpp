7f5145b33f13adc93c73e636c647a8eb95a4d904
Trevor Knott <Tknott95@hotmail.com>
2020-04-13 03:29:17 -0600

    BsGLTest - Cub3Z

--- 3dGames/Cub3Z/src/main.cpp
+++ 3dGames/Cub3Z/src/main.cpp
@@ -4,7 +4,6 @@
 /* forLINUX: -lsfml-graphics -lsfml-window -lsfml-system -lGL */
 int main() {
   cout << "\n App Initialized... \n" << endl;
-  // std::srand(static_cast<unsigned>(time(NULL)));
 
   Game game;
   while(game.IsOpen()) {
