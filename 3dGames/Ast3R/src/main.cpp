18fc63cc3bdf90852d14800f4d4763f2f32ad5b8
Trevor Knott <Tknott95@hotmail.com>
2020-04-15 05:12:18 -0600

    KeyPolling glfw | Ast3R

--- 3dGames/Ast3R/src/main.cpp
+++ 3dGames/Ast3R/src/main.cpp
@@ -6,6 +6,20 @@
 
 using namespace std;
 
+void keyPolling(GLFWwindow* window) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+      printf("\n WindowClosing\n");
+      glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS) {
+      printf("\n  zPressed, bitch");
+    }
+    if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS) {
+      printf("\n  xPressed, bitch");
+    }
+}
+
 int main() {
   cout << "\n appInitialized...\n" << endl;
   if(!glfwInit()) {
@@ -31,10 +45,7 @@ int main() {
     glfwSwapBuffers(window);
     glfwPollEvents();
 
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-      printf("\n WindowClosing\n");
-      glfwSetWindowShouldClose(window, GL_TRUE);
-    }
+    keyPolling(window);
   }
 
 
