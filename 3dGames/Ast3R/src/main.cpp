2614e9755acbd54a459035b5eaba9920cc575c37
Trevor Knott <Tknott95@hotmail.com>
2020-04-14 22:33:15 -0600

    glfwKeyInput - Ast3R

--- 3dGames/Ast3R/src/main.cpp
+++ 3dGames/Ast3R/src/main.cpp
@@ -2,16 +2,41 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <thread>
 
 using namespace std;
 
 int main() {
-  cout << "\n   appInitialized...\n" << endl;
+  cout << "\n appInitialized...\n" << endl;
   if(!glfwInit()) {
     printf("\n glfwInit() FAILED\n");
     return -1;
   }
 
   glfwInit();
+  // this_thread::sleep_for(chrono::seconds(1));
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+  GLFWwindow* window = glfwCreateWindow(900, 700, "OpenGL", nullptr, nullptr); // Windowed
+  // GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGL", glfwGetPrimaryMonitor(), nullptr); // Fullscreen
+
+  glfwMakeContextCurrent(window);
+  // glfwTerminate();
+
+  while(!glfwWindowShouldClose(window)) {
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+      printf("\n WindowClosing\n");
+      glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+  }
+
+
   return 0;
 }
