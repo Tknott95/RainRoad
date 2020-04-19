26a92fb7062e0a08f4113f9e6757450552dfdd40
Trevor Knott <Tknott95@hotmail.com>
2020-04-19 12:53:00 -0600

    BufferCleanup, destructorTyne | Ast3R

--- 3dGames/Ast3R/src/main.cpp
+++ 3dGames/Ast3R/src/main.cpp
@@ -35,8 +35,7 @@ void initWindow() {
 const char* vertexSource = R"glsl(
     #version 150 core
     in vec2 position;
-    void main()
-    {
+    void main() {
       gl_Position = vec4(position, 0.0, 1.0);
     }
   )glsl";
@@ -44,8 +43,7 @@ const char* vertexSource = R"glsl(
   const char* fragmentSource = R"glsl(
   #version 150 core
   out vec4 outColor;
-  void main()
-  {
+  void main() {
     outColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);    }
   )glsl";
 
