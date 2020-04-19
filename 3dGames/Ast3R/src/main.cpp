f362d80381491f1becd716f9eba32e91b8119efb
Trevor Knott <Tknott95@hotmail.com>
2020-04-18 20:11:49 -0600

    KeysAbstraction | Ast3R

--- 3dGames/Ast3R/src/main.cpp
+++ 3dGames/Ast3R/src/main.cpp
@@ -31,9 +31,9 @@ int main() {
   glewInit();
 
   float vertices[] = {
-    0.0f, 0.5f, // Vertex 1 (X, Y)
-    0.5f, -0.5f, // Vertex 2 (X, Y)
-    -0.5f, -0.5f // Vertex 3 (X, Y)
+    0.0f, 0.5f,
+    0.5f, -0.5f,
+    -0.5f, -0.5f
   };
 
   GLuint vBuff;
