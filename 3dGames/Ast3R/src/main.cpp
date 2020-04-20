71447b81207c938a812b0ed5b19786d295668873
Trevor Knott <Tknott95@hotmail.com>
2020-04-20 00:27:14 -0600

    REMOVED stb_image.h (third party .h) init() | Ast3RZ

--- 3dGames/Ast3R/src/main.cpp
+++ 3dGames/Ast3R/src/main.cpp
@@ -4,9 +4,10 @@
 // #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <thread>
+#include <string>
+#include <fstream>
+#include <sstream>
 #include "headers/keys.h"
-#define STB_IMAGE_IMPLEMENTATION
-#include "headers/stb_image.h"
 
 using namespace std;
 
@@ -124,6 +125,19 @@ void glInit() {
   // texture attribute
   glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
   glEnableVertexAttribArray(2);
+
+      // load and create a texture 
+    // -------------------------
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
 
   glUseProgram(shaderProgram);
 
