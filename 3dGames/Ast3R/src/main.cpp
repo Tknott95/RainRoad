aae3f7ca82e60fa28e31d9393e9dd1ea9e81b770
Trevor Knott <Tknott95@hotmail.com>
2020-04-22 13:35:38 -0600

    transformations03 w/ time and marble.jpg | Ast3RZ

--- 3dGames/Ast3R/src/main.cpp
+++ 3dGames/Ast3R/src/main.cpp
@@ -145,30 +145,24 @@ void glInit() {
   glEnableVertexAttribArray(2);
 
   /* TEXTURE INIT HERE */
-
-
-
-  // glBindVertexArray(0);
-    int width, height, nrChannels;
-    // The FileSystem::getPath(...) is part of the GitHub repository so we can find files on any IDE/platform; replace it with your own image path.
-    // resources/textures/container.jpg
-    unsigned char *data = stbi_load("resources/textures/marble.jpg", &width, &height, &nrChannels, 0);
+  int width, height, nrChannels;
+  unsigned char *data = stbi_load("resources/textures/marble.jpg", &width, &height, &nrChannels, 0);
     // unsigned int texture; MADE GLOBAL
-    glGenTextures(1, &texture0);
-    glBindTexture(GL_TEXTURE_2D, texture0); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+  glGenTextures(1, &texture0);
+  glBindTexture(GL_TEXTURE_2D, texture0); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
     // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	// set texture wrapping to GL_REPEAT (default wrapping method)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    if (data) {
-      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-      glGenerateMipmap(GL_TEXTURE_2D);
-    } else {
-      cout << "Failed to load texture" << endl;
-    }
-    stbi_image_free(data);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    cout << "Failed to load texture" << endl;
+  }
+  stbi_image_free(data);
 
   /* endTextureInit */
 
