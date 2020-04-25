6744a3ed1c5a7c21369bec04263b4a4347ce4b70
Trevor Knott <Tknott95@hotmail.com>
2020-04-25 15:45:27 -0600

    Shader->compile calling w/out crash | gLit_Lighting

--- 3dLand/gLit_Lighting/src/ctrls/draw.cpp
+++ 3dLand/gLit_Lighting/src/ctrls/draw.cpp
@@ -5,66 +5,66 @@ void Draw::init() {
   this->matShader.compile("src/shaders/material/material.vs", "src/shaders/material/material.fs");
   this->lightShader.compile("src/shaders/light/light.vs", "src/shaders/light/light.fs");
 
-  // glGenVertexArrays(1, &cubeVAO);
-  // glGenBuffers(1, &vbo);
-  // glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  // glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
-  // glBindVertexArray(cubeVAO);
+  glGenVertexArrays(1, &cubeVAO);
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+  glBindVertexArray(cubeVAO);
 
-  // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-  // glEnableVertexAttribArray(0);
-  // glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-  // glEnableVertexAttribArray(1);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
 
-  // glGenVertexArrays(1, &lightVAO);
-  // glBindVertexArray(lightVAO);
+  glGenVertexArrays(1, &lightVAO);
+  glBindVertexArray(lightVAO);
 
-  // glBindBuffer(GL_ARRAY_BUFFER, vbo);
-  // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-  // glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
 
-  // lightPos = glm::vec3(1.2f, 1.0f, 2.0f);
+  lightPos = glm::vec3(1.2f, 1.0f, 2.0f);
 }
 
 void Draw::update() {
-  // materialShader.use();
-  // materialShader.setVec3("light.position", lightPos);
-  // //materialShader.setVec3("viewPos", camera.Position);
+  matShader.use();
+  matShader.setVec3("light.position", lightPos);
+  //matShader.setVec3("viewPos", camera.Position);
 
-  // glm::vec3 lightColor;
-  // lightColor.x = sin(glfwGetTime() * 2.0f);
-  // lightColor.y = sin(glfwGetTime() * 0.7f);
-  // lightColor.z = sin(glfwGetTime() * 1.3f);
-  // glm::vec3 diffuseColor = lightColor   * glm::vec3(0.5f);
-  // glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f);
-  // materialShader.setVec3("light.ambient", ambientColor);
-  // materialShader.setVec3("light.diffuse", diffuseColor);
-  // materialShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+  glm::vec3 lightColor;
+  lightColor.x = sin(glfwGetTime() * 2.0f);
+  lightColor.y = sin(glfwGetTime() * 0.7f);
+  lightColor.z = sin(glfwGetTime() * 1.3f);
+  glm::vec3 diffuseColor = lightColor   * glm::vec3(0.5f);
+  glm::vec3 ambientColor = diffuseColor * glm::vec3(0.2f);
+  matShader.setVec3("light.ambient", ambientColor);
+  matShader.setVec3("light.diffuse", diffuseColor);
+  matShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
 
-  // materialShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
-  // materialShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
-  // materialShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
-  // materialShader.setFloat("material.shininess", 32.0f);
+  matShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
+  matShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
+  matShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
+  matShader.setFloat("material.shininess", 32.0f);
 
-  // glm::mat4 projection = glm::mat4(1.0f); //= glm::perspective(glm::radians(camera.Zoom), (float)_screenWidth / (float)_screenHeight, 0.1f, 100.0f);
-  // glm::mat4 view = glm::mat4(1.0f); //camera.GetViewMatrix();
-  // materialShader.setMat4("projection", projection);
-  // materialShader.setMat4("view", view);
+  glm::mat4 projection = glm::mat4(1.0f); //= glm::perspective(glm::radians(camera.Zoom), (float)_screenWidth / (float)_screenHeight, 0.1f, 100.0f);
+  glm::mat4 view = glm::mat4(1.0f); //camera.GetViewMatrix();
+  matShader.setMat4("projection", projection);
+  matShader.setMat4("view", view);
 
-  // glm::mat4 model = glm::mat4(1.0f);
-  // materialShader.setMat4("model", model);
+  glm::mat4 model = glm::mat4(1.0f);
+  matShader.setMat4("model", model);
 
-  // glBindVertexArray(cubeVAO);
-  // glDrawArrays(GL_TRIANGLES, 0, 36);
+  glBindVertexArray(cubeVAO);
+  glDrawArrays(GL_TRIANGLES, 0, 36);
 
-  // lightShader.use();
-  // lightShader.setMat4("projection", projection);
-  // lightShader.setMat4("view", view);
-  // model = glm::mat4(1.0f);
+  lightShader.use();
+  lightShader.setMat4("projection", projection);
+  lightShader.setMat4("view", view);
+  model = glm::mat4(1.0f);
   // model = glm::translate(model, lightPos);
   // model = glm::scale(model, glm::vec3(0.2f));
-  // lightShader.setMat4("model", model);
+  lightShader.setMat4("model", model);
 
-  // glBindVertexArray(lightVAO);
-  // glDrawArrays(GL_TRIANGLES, 0, 36);
+  glBindVertexArray(lightVAO);
+  glDrawArrays(GL_TRIANGLES, 0, 36);
 }
--- 3dLand/gLit_Lighting/src/headers/draw.h
+++ 3dLand/gLit_Lighting/src/headers/draw.h
@@ -1,10 +1,12 @@
 // #include <string>
 #include "shader.h"
+#include <GLFW/glfw3.h>
+
 /* @TODO hand make shaders for lighting then create iwn shader class w/ own methodology */
 class Draw {
   private:
-    // unsigned int vbo, cubeVAO, lightVAO;
-    // glm::vec3 lightPos;
+    unsigned int vbo, cubeVAO, lightVAO;
+    glm::vec3 lightPos;
     Shader matShader;
     Shader lightShader;
 
