b1fbf052cc02f69081a170de00f6d74465364c05
Trevor Knott <Tknott95@hotmail.com>
2020-04-25 07:47:18 -0600

    Draw class | gLit_Lighting - hotfix TksGlfwFramework

--- 3dLand/TKsGlfwFramework/src/ctrls/window.cpp
+++ 3dLand/TKsGlfwFramework/src/ctrls/window.cpp
@@ -2,8 +2,11 @@
 
 Window::Window() { 
   if(!glfwInit()) printf("\n\e[0;31;40m glfwInit() FAILED \e[0m\n");
+  if(!glewInit()) printf("\n\e[0;31;40m GlewInit FAILED \e[0m\n");
 
   glfwInit();
+  glewInit();
+
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
@@ -11,10 +14,7 @@ Window::Window() {
 
   this->_window = glfwCreateWindow(_screenWidth, _screenHeight, "LitLighting", nullptr, nullptr);
 
-  if(!glewInit()) printf("\n\e[0;31;40m GlewInit FAILED \e[0m\n");
   glfwMakeContextCurrent(_window);
-
-  glewInit();
 };
 
 Window::~Window() {
