8345d14f49c36a0ffea9e7690b80fca392000aa4
Trevor Knott <Tknott95@hotmail.com>
2020-04-25 19:03:56 -0600

    Draw is now a pointer, shaderLighing 1/2 | gLit_Lighting

--- 3dLand/gLit_Lighting/src/ctrls/draw.cpp
+++ 3dLand/gLit_Lighting/src/ctrls/draw.cpp
@@ -7,21 +7,21 @@ void Draw::init() {
   materialShader.compile("src/shaders/material/material.vs", "src/shaders/material/material.fs");
   lampShader.compile("src/shaders/light/light.vs", "src/shaders/light/light.fs");
 
-  // glGenVertexArrays(1, &cubeVAO);
-  // glGenBuffers(1, &VBO);
+  glGenVertexArrays(1, &cubeVAO);
+  glGenBuffers(1, &VBO);
 
-  // glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  // glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
   
-  // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-  // glEnableVertexAttribArray(0);
-  // glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-  // glEnableVertexAttribArray(1);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
   
-  // glGenVertexArrays(1, &lightVAO);
-  // glBindVertexArray(lightVAO);
+  glGenVertexArrays(1, &lightVAO);
+  glBindVertexArray(lightVAO);
 
-  // glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
   // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
   // glEnableVertexAttribArray(0);
 }
