e867c8fb934f4eb9c57e9f1153b72b76d4ef7f16
Trevor Knott <Tknott95@hotmail.com>
2020-04-28 16:47:55 -0600

    SkyboxFunction in TextureClass | TKsGlfwFramework

--- 3dLand/TKsFrameworkGlfw/src/ctrls/texture.cpp
+++ 3dLand/TKsFrameworkGlfw/src/ctrls/texture.cpp
@@ -1,18 +1,18 @@
 #include "../headers/texture.h"
 
-unsigned int Texture::load(const char *path) {
-  unsigned int textureID;
-  glGenTextures(1, &textureID);
+uint Texture::load(const char *path) {
+  uint texID;
+  glGenTextures(1, &texID);
 
-  int width, height, nrComponents;
-  unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+  int width, height, numChannels;
+  unsigned char *data = stbi_load(path, &width, &height, &numChannels, 0);
   if (data) {
     GLenum format;
-    if (nrComponents == 1) format = GL_RED;
-    else if (nrComponents == 3) format = GL_RGB;
-    else if (nrComponents == 4) format = GL_RGBA;
+    if (numChannels == 1) format = GL_RED;
+    else if (numChannels == 3) format = GL_RGB;
+    else if (numChannels == 4) format = GL_RGBA;
 
-    glBindTexture(GL_TEXTURE_2D, textureID);
+    glBindTexture(GL_TEXTURE_2D, texID);
     glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
 
@@ -27,7 +27,36 @@ unsigned int Texture::load(const char *path) {
     stbi_image_free(data);
   }
 
-  printf("\n  \e[1;95;40m  Texture(%d) Loaded:\e[0;33;40m %s \e[0m\n", textureID, path);
+  printf("\n  \e[1;95;40m  Texture(%d) Loaded:\e[0;33;40m %s \e[0m\n", texID, path);
 
-  return textureID;
+  return texID;
+}
+
+uint Texture::loadSkybox(std::vector<std::string> _sbFaces) {
+  uint texID;
+  glGenTextures(1, &texID);
+  glBindTexture(GL_TEXTURE_CUBE_MAP, texID);
+
+  int width, height, numChannels;
+  for(uint i = 0; i < _sbFaces.size(); i++) {
+    u_char *data = stbi_load(_sbFaces[i].c_str(), &width, &height, &numChannels, 0);
+
+    if(data) {
+      glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+      stbi_image_free(data);
+    } else {
+      std::cout << "\e[0;31;40m Skybox texture failed to load: " << _sbFaces[i] << "\e[0m" <<std::endl;
+      stbi_image_free(data);
+    }
+    
+    printf("\n  \e[1;95;40m  Texture(%d) Loaded:\e[0;33;40m %s \e[0m\n", texID, _sbFaces[i].c_str());
+  }
+
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+  return texID;
 }
--- 3dLand/TKsFrameworkGlfw/src/headers/texture.h
+++ 3dLand/TKsFrameworkGlfw/src/headers/texture.h
@@ -1,10 +1,12 @@
+#include <iostream>
+#include <vector>
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "third_party/stb_image.h"
-
 #include <GL/glew.h>
-#include <iostream>
 
 class Texture {
   public:
-    unsigned int load(const char *path);
+    uint load(const char *path);
+    uint loadSkybox(std::vector<std::string>);
 };
