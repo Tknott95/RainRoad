e348880a57236aa32d1a50b557fea5ecd47d1c93
Trevor Knott <Tknott95@hotmail.com>
2020-04-28 16:42:52 -0600

    Skybox done - skyboxFunc modularized | gLit_Lighting

--- 3dLand/gLit_Skyboxes/src/ctrls/texture.cpp
+++ 3dLand/gLit_Skyboxes/src/ctrls/texture.cpp
@@ -4,13 +4,13 @@ uint Texture::load(const char *path) {
   uint texID;
   glGenTextures(1, &texID);
 
-  int width, height, nrComponents;
-  unsigned char *data = stbi_load(path, &width, &height, &nrComponents, 0);
+  int width, height, numChannels;
+  unsigned char *data = stbi_load(path, &width, &height, &numChannels, 0);
   if (data) {
     GLenum format;
-    if (nrComponents == 1) format = GL_RED;
-    else if (nrComponents == 3) format = GL_RGB;
-    else if (nrComponents == 4) format = GL_RGBA;
+    if (numChannels == 1) format = GL_RED;
+    else if (numChannels == 3) format = GL_RGB;
+    else if (numChannels == 4) format = GL_RGBA;
 
     glBindTexture(GL_TEXTURE_2D, texID);
     glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
@@ -33,7 +33,6 @@ uint Texture::load(const char *path) {
 }
 
 uint Texture::loadSkybox(std::vector<std::string> _sbFaces) {
-  /* @TODO move into my texture class */
   uint texID;
   glGenTextures(1, &texID);
   glBindTexture(GL_TEXTURE_CUBE_MAP, texID);
