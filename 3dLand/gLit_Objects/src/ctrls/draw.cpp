9a33d67530db6b39f49cbf7428eb846e8552dee0
Trevor Knott <tknott95@hotmail.com>
2020-04-30 23:58:19 -0600

    Objs Drawing Dynamically | gLit_Objects

--- 3dLand/gLit_Objects/src/ctrls/draw.cpp
+++ 3dLand/gLit_Objects/src/ctrls/draw.cpp
@@ -12,7 +12,7 @@ void Draw::init() {
     "assets/skybox/back.jpg"
   };
 
-  objData = objLoader.load("assets/objects/cubeExample.obj");
+  objData = objLoader.load("assets/objects/oddShape.obj");
 
   skyboxShader.compile("assets/shaders/skybox.vs", "assets/shaders/skybox.fs");
   objShader.compile("assets/shaders/obj.vs", "assets/shaders/obj.fs");
@@ -20,14 +20,14 @@ void Draw::init() {
   glGenVertexArrays(1, &objVAO);
   glGenBuffers(1, &objVBO);
   glBindVertexArray(objVAO);
-  glBindBuffer(GL_ARRAY_BUFFER, objVBO);
-  glBufferData(GL_ARRAY_BUFFER, objData.vertices.size()-1 * sizeof(glm::vec3), &objData.vertices[0], GL_STATIC_DRAW);
-  // glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, objVBO); //  * sizeof(glm::vec3)
+  glBufferData(GL_ARRAY_BUFFER, objData.vertices.size(), &objData.vertices[0], GL_STATIC_DRAW);
+  glEnableVertexAttribArray(0);
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-  // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, objEBO);
-  // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(objData.vertIndices), objData.vertIndices, GL_STATIC_DRAW);
-  std::cout << objData.vertices.size()-1 << std::endl;
-
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, objEBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(objData.vertIndices), &objData.vertIndices, GL_STATIC_DRAW);
+  std::cout << objData.vertices.size() << std::endl;
+  // glBindVertexArray(objVAO);
 
   glGenVertexArrays(1, &skyboxVAO);
   glGenBuffers(1, &skyboxVBO);
@@ -46,7 +46,6 @@ void Draw::init() {
 
 void Draw::update(Camera* camera, ivec2 screenSize) {
   glDepthFunc(GL_LEQUAL);
-  glDepthMask(GL_FALSE);
   skyboxShader.use();
 
   mat4 view = mat4(1.0f);
@@ -59,16 +58,18 @@ void Draw::update(Camera* camera, ivec2 screenSize) {
   glActiveTexture(GL_TEXTURE0);
   glBindTexture(GL_TEXTURE_CUBE_MAP, sbTexID);
   glDrawArrays(GL_TRIANGLES, 0, 36);
-  glDepthFunc(GL_LESS);
-  glDepthMask(GL_TRUE);
+  glBindVertexArray(0);
 
   objShader.use();
   // mat4 objColor = mat4(1.0f, 1.0f, 1.0f, 1.0f);
   // objShader.setMat4("ourColor", objColor);
   glBindVertexArray(objVAO); //objData.vertices.size()-1
-  uint objVertSize =  objData.vertices.size()-1;
-  glDrawArrays(GL_TRIANGLES, 0 ,7);
-  glBindVertexArray(objVAO);
+  // glActiveTexture(GL_TEXTURE0);
+  // glBindTexture(GL_TEXTURE_CUBE_MAP, sbTexID);
+  uint objVertSize =  objData.vertices.size();
+  glDrawArrays(GL_TRIANGLES, 0 , objVertSize);
+  glBindVertexArray(0); // glBindVertexArray(objVAO);AO);
+  glDepthFunc(GL_LESS);
 }
 
 void Draw::deallocate() {
--- 3dLand/gLit_Objects/src/headers/obj_loader.h
+++ 3dLand/gLit_Objects/src/headers/obj_loader.h
@@ -8,6 +8,7 @@ struct ObjStruct { /* this will have to be an external header OR created twice *
   std::vector<glm::vec3> vertices;
   std::vector<glm::vec2> uvs;
   std::vector<glm::vec3> normals; 
+  std::vector<uint> vertIndices;
 };
 
 class ObjectLoader {
