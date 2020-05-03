68c651ef634b6059a8316bad7ddf5b4ad26477b0
Trevor Knott <tknott95@hotmail.com>
2020-05-02 21:21:41 -0600

    NotesForPrep b4 new coordData inserts | gLit_Objects

--- 3dLand/gLit_Objects/src/ctrls/draw.cpp
+++ 3dLand/gLit_Objects/src/ctrls/draw.cpp
@@ -18,6 +18,7 @@ void Draw::init() {
   skyboxShader.compile("assets/shaders/skybox.vs", "assets/shaders/skybox.fs");
   objShader.compile("assets/shaders/obj.vs", "assets/shaders/obj.fs");
 
+  /******** binding obj attribs ********/
   glGenVertexArrays(1, &objVAO);
   glGenBuffers(1, &objVBO);
   glGenBuffers(1, &objEBO);
@@ -25,10 +26,18 @@ void Draw::init() {
   glBindBuffer(GL_ARRAY_BUFFER, objVBO); //  * sizeof(glm::vec3)
   glBufferData(GL_ARRAY_BUFFER, objData.vertices.size(), &objData.vertices[0], GL_STATIC_DRAW);
   glEnableVertexAttribArray(0);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glVertexAttribPointer(
+    0, /* matches layout in hader */
+    3, /* size */
+    GL_FLOAT, /* type */
+    GL_FALSE, /* isNormalized? */
+    3 * sizeof(float), /* stride, matches my data xyz|3 */
+    (void*)0 /* array buffer offset */
+  );
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, objEBO);
   glBufferData(GL_ELEMENT_ARRAY_BUFFER, objData.vertIndices.size() * sizeof(uint), &objData.vertIndices[0], GL_STATIC_DRAW);
 
+  /******** binding skybox attribs *******/
   glGenVertexArrays(1, &skyboxVAO);
   glGenBuffers(1, &skyboxVBO);
   glBindVertexArray(skyboxVAO);
@@ -42,7 +51,6 @@ void Draw::init() {
   sbTexID = texture.loadSkybox(sbFaces);
   objTexID = texture.load("assets/textures/box_weave.png");
 
-  // glDepthMask(GL_FALSE);
   skyboxShader.use();
   skyboxShader.setInt("skybox", 0);
 }
@@ -54,7 +62,7 @@ void Draw::update(Camera* camera, ivec2 screenSize) {
   mat4 transform = mat4(1.0f);
   mat4 projection = perspective(radians(camera->Zoom), (float)screenSize.x / (float)screenSize.y, 0.1f, 100.f);
   mat4 view = camera->GetViewMatrix();
-  transform = translate(transform, glm::vec3(0.0f, 5.0f, 5.0f));
+  transform = translate(transform, glm::vec3(0.0f, 2.0f, 3.0f));
 
   objShader.setMat4("model", model);
   objShader.setMat4("view", view);
@@ -67,7 +75,6 @@ void Draw::update(Camera* camera, ivec2 screenSize) {
   /* @TODO need texture coordinates imported in */
   glActiveTexture(GL_TEXTURE1);
   glBindTexture(GL_TEXTURE1, objTexID);
-  // glDrawArrays(GL_TRIANGLES, 0 , objData.vertices.size());
   glDrawElements(GL_TRIANGLES, objData.vertIndices.size(), GL_UNSIGNED_INT, 0);
   glBindVertexArray(0);
   /************* OBJ DRAWING FINISHED **********************/
