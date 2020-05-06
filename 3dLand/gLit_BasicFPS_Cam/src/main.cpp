c7e140b0025d8afc72463877ed8d87810c606b5d
Trevor Knott <Tknott95@hotmail.com>
2020-05-05 23:22:14 -0600

    rm main | gLit_FPSCAM

--- 3dLand/gLit_BasicFPS_Cam/src/main.cpp
+++ 3dLand/gLit_BasicFPS_Cam/src/main.cpp
@@ -1,9 +1,4 @@
 #include <iostream>
-// #define GLEW_STATIC replaced w/ cli:  -DGLEW_STATIC
-// #include <GL/glew.h>
-// #include <GLFW/glfw3.h>
-// #include <glm/glm.hpp>
-// #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
 #include <thread>
