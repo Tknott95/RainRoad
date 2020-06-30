91911f71ce93ef13bb75688b6dba9048fa289336
Trevor Knott <Tknott95@hotmail.com>
2020-06-30 04:58:40 +0000

    run.sh 4 LINUX -> TrisGround

--- TrisGround/run.sh
+++ TrisGround/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-g++ -Wall main.cpp -lglfw3 -lopengl32 -lgdi32 -luser32 -lkernel32 -lglfw -lGL -lglfw3dll -lopengl32 -o main && ./main.exe
+g++ -Wall main.cpp -lglfw -lGL -o bin/main && ./bin/main
+# g++ -Wall main.cpp -lglfw3 -lopengl32 -lgdi32 -luser32 -lkernel32 -lglfw -lGL -lglfw3dll -lopengl32 -o main && ./main.exe
