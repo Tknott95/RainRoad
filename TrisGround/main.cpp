b2bcced25c7a90b97357cb94568dafb1811162fa
Tknott95 <tknott95@hotmail.com>
2020-06-30 04:23:58 -0600

    .sh script run.sh -> TrisGround

--- TrisGround/main.cpp
+++ TrisGround/main.cpp
@@ -1,5 +1,8 @@
 #include<iostream>
 #include<vector>
+
+#include <GLFW/glfw3.h>
+
 using namespace std;
 
 class RefactorMe {
@@ -53,7 +56,7 @@ int main() {
 
   printf("\n - cubeMesh tri coords  - \n");
   for (auto tri : refactorMe.cubeMesh.tris) {
-    // triangle newtri;
+    // Here we would draw each triangle coord, (x,y,z), via: clockwise motion as the coords are orientated that way.
     printf("{(%.2f, %.2f, %.2f),  (%.2f, %.2f, %.2f),  (%.2f, %.2f, %.2f)} \n", tri.coord[0].x, tri.coord[0].y, tri.coord[0].z, tri.coord[1].x, tri.coord[1].y, tri.coord[1].z, tri.coord[2].x, tri.coord[2].y, tri.coord[2].z);
   }
   printf("\n");
---
+++ TrisGround/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+g++ -Wall main.cpp -lglfw3 -lopengl32 -lgdi32 -luser32 -lkernel32 -lglfw -lGL -lglfw3dll -lopengl32 -o main && ./main.exe
