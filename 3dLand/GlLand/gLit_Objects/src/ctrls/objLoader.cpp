a0825bac004d0509420b217d0e0c5a642e068843
Trevor Knott <Tknott95@hotmail.com>
2020-07-01 14:42:35 +0000

    LoggerFile for encodedObj DEBUGGING -> gLit_Objects

--- 3dLand/GlLand/gLit_Objects/src/ctrls/objLoader.cpp
+++ 3dLand/GlLand/gLit_Objects/src/ctrls/objLoader.cpp
@@ -1,5 +1,5 @@
 #include "../headers/objLoader.h"
-
+#include <fstream>
 /******************************
   USING
     v - vertices - x y z
@@ -15,6 +15,8 @@ EncodedObj ObjectLoader::load(const char* objPath) {
   FILE *data = fopen(objPath, "r");
   if(!data) printf("\n\e[0;31;40m OBJECT NOT LOADING\e[0m"); /* change this to be under the return of the if(data) as it is da waay */
 
+  ofstream objFileForLogger;
+  objFileForLogger.open ("assets/logs/encodedObjLogger.txt");
   while(data) { /* while so it reads every lang rather then an if */
     char header[256];
     int reader = fscanf(data, "%s", header);
@@ -30,6 +32,7 @@ EncodedObj ObjectLoader::load(const char* objPath) {
     else if(strcmp(header, "v") == 0) {
       vec3 vertex;
       fscanf(data, "%f %f %f \n", &vertex.x, &vertex.y, &vertex.z);
+      objFileForLogger << "v " << vertex.x << ", " << vertex.y << ", " << vertex.z <<"\n";
       encodedObj.vertices.push_back(vertex);
     }
     else if(strcmp(header, "vt") == 0) {
@@ -46,6 +49,8 @@ EncodedObj ObjectLoader::load(const char* objPath) {
       uint vIndex[3], uvIndex[3], normIndex[3];
       int dataAmt = fscanf(data, "%i/%i/%i %i/%i/%i %i/%i/%i\n", &vIndex[0], &uvIndex[0], &normIndex[0], &vIndex[1], &uvIndex[1], &normIndex[1], &vIndex[2], &uvIndex[2], &normIndex[2]);
       if(dataAmt != 9) printf("\n\e[0;31;40m OBJ(face) DATA TOO LARGE(!9)\e[0m");
+
+      objFileForLogger << "i-ces 1(" << vIndex[0] << ") 2(" << vIndex[1] << ") 3(" << vIndex[2] <<") \n";
 
       vertIndices.push_back(vIndex[0]);
       vertIndices.push_back(vIndex[1]);
@@ -72,6 +77,8 @@ EncodedObj ObjectLoader::load(const char* objPath) {
 
   printf("\n  \e[0;94;40m  Object Loaded: \e[0;33;40m %s \n    v(%d) vIndices(%d) \e[0m\n",
     objPath, encodedObj.vertices.size(), vertIndices.size());
+  
+  objFileForLogger.close();
 
   return encodedObj;
 };
--- 3dLand/GlLand/gLit_Objects/zrv.sh
+++ 3dLand/GlLand/gLit_Objects/zrv.sh
@@ -1,8 +1,20 @@
 # file:///home/x4/Downloads/Modern%20OpenGL%20Guide.pdf
+_LOGGER_FILE_PATH="assets/logs/encodedObjLogger.txt"
 _S="./src"
 _C="$_S/ctrls"
 _CPPs=" 
   $_C/window.cpp $_C/keys.cpp $_C/camera.cpp
   $_C/draw.cpp $_C/shader.cpp $_C/texture.cpp $_C/objLoader.cpp"
 
-g++ $_S/$1.cpp $_CPPs -o ./runtime/$1 -lglfw -lGLEW -DGLEW_STATIC -lGL  && ./runtime/$1
+echo "\n##### - Logger Init() -"
+FILE=/etc/resolv.conf
+if test -f "$FILE"; then
+  echo "Cleaning logger files at: $FILE"
+  rm -f assets/logs/encodedObjLogger.txt
+fi
+
+echo "\n############### - Compiling the binary \n"
+g++ $_S/$1.cpp $_CPPs -o ./runtime/$1 -lglfw -lGLEW -DGLEW_STATIC -lGL  # && ./runtime/$1
+
+echo "\n######################## - Running the binary \n"
+./runtime/$1
