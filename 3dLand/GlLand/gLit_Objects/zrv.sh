5c66114b47d4b28e23b25c9ca309d6bcfe559968
Trevor Knott <Tknott95@hotmail.com>
2020-07-01 14:52:32 +0000

    LoggerFile for encodedObj DEBUGGING w/ live file check and cleaning -> gLit_Objects

--- 3dLand/GlLand/gLit_Objects/zrv.sh
+++ 3dLand/GlLand/gLit_Objects/zrv.sh
@@ -1,4 +1,3 @@
-# file:///home/x4/Downloads/Modern%20OpenGL%20Guide.pdf
 _LOGGER_FILE_PATH="assets/logs/encodedObjLogger.txt"
 _S="./src"
 _C="$_S/ctrls"
@@ -6,15 +5,15 @@ _CPPs="
   $_C/window.cpp $_C/keys.cpp $_C/camera.cpp
   $_C/draw.cpp $_C/shader.cpp $_C/texture.cpp $_C/objLoader.cpp"
 
-echo "\n##### - Logger Init() -"
+echo "    - Logger Init() -"
 FILE=/etc/resolv.conf
-if test -f "$FILE"; then
-  echo "Cleaning logger files at: $FILE"
+if test -f "$_LOGGER_FILE_PATH"; then
+  echo "    - Cleaning logger files at: $_LOGGER_FILE_PATH"
   rm -f assets/logs/encodedObjLogger.txt
 fi
 
-echo "\n############### - Compiling the binary \n"
-g++ $_S/$1.cpp $_CPPs -o ./runtime/$1 -lglfw -lGLEW -DGLEW_STATIC -lGL  # && ./runtime/$1
+echo "    - COMPILING BINARY"
+g++ $_S/$1.cpp $_CPPs -o ./runtime/$1 -lglfw -lGLEW -DGLEW_STATIC -lGL # && ./runtime/$1
 
-echo "\n######################## - Running the binary \n"
+echo "    - RUNNING BINARY"
 ./runtime/$1
