edcce75b48240d94bf2752a90ff2783249b09d86
Tknott95 <tknott95@hotmail.com>
2020-07-05 06:11:32 -0600

    m2x2 w/ inits() -> param passing()  => LinAlgLib

--- LinAlgLib/main.cpp
+++ LinAlgLib/main.cpp
@@ -13,11 +13,11 @@ int main() {
   printf("v01(%f, %f)\n", v01.x, v01.y);
 
   m2x2<float> m00;
-  float f2[2] = {2.f, 2.f};
-  float f3[2] = {2.f, 2.f};
-  m2x2<float> m01 = {f2, f3};
+  float x0[2] = {2.f, 2.f};
+  float y0[2] = {4.f, 4.f};
+  m2x2<float> m01 = {x0, y0};
   printf("\nm00[[%f, %f], [%f, %f])\n", m00.x[0], m00.y[0], m00.x[1], m00.y[1]);
-  //  printf("m01[[%f, %f], [%f, %f])\n", m01.x[0], m01.y[0], m01.x[1], m01.y[1]);
+  printf("m01[[%f, %f], [%f, %f])\n", m01.x[0], m01.y[0], m01.x[1], m01.y[1]);
 
   return 0;
 }
