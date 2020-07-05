b62724424d0769821de11ce8d17c5a9a006e706a
Tknott95 <tknott95@hotmail.com>
2020-07-05 06:17:13 -0600

    m2x2 w/ adv operators  -> LinAlgLib

--- LinAlgLib/m.h
+++ LinAlgLib/m.h
@@ -39,5 +39,4 @@ struct m2x2 {
   m2x2& operator /= (const m2x2& other) { /* maybe loop? */
     this->x[0] /= other.x[0]; this->y[0] /= other.y[0]; this->x[1] /= other.x[1]; this->y[1] /= other.y[1]; return *this;
   }
-
 };
