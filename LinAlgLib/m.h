45dfaf43cdb12dd2f2497f6bb04dd291a6fd9c40
Tknott95 <tknott95@hotmail.com>
2020-07-05 14:29:52 -0600

    m3x3 -> LinAlgLib

--- LinAlgLib/m.h
+++ LinAlgLib/m.h
@@ -12,7 +12,6 @@ struct m2x2 {
 
   m2x2() : val{0.f, 0.f, 1.f, 1.f} {}
   m2x2(T _x1, T _y1, T _x2, T _y2) : val{_x1, _y1, _x2, _y2} {
-    /* makes things easier, will rmv if need extra memory */
     x[0] = val[0];
     y[0] = val[1];
     x[1] = val[2];
@@ -42,7 +41,6 @@ struct m3x3 {
 
   m3x3() : val{0.f, 0.f, 0.f, 1.f, 1.f, 1.f, 2.f, 2.f, 2.f} {}
   m3x3(T _x1, T _y1, T _z1, T _x2, T _y2, T _z2, T _x3, T _y3, T _z3) : val{_x1, _y1, _z1, _x2, _y2, _z2, _x3, _y3, _z3} {
-    /* makes things easier, will rmv if need extra memory */
     x[0] = val[0];
     y[0] = val[1];
     z[0] = val[2];
@@ -57,16 +55,16 @@ struct m3x3 {
   m3x3 operator + (const m3x3 other) {
     return m3x3(this->x[0] + other.x[0], this->y[0] + other.y[0], this->z[0] + other.z[0], this->x[1] + other.x[1], this->y[1] + other.y[1], this->z[1] + other.z[1], this->x[2] + other.x[2], this->y[2] + other.y[2], this->z[2] + other.z[2]);
   }
-  // m2x2 operator - (const m2x2 other) {
-  //   return m2x2(this->x[0] - other.x[0], this->y[0] - other.y[0], this->x[1] - other.x[1], this->y[1] - other.y[1]);
-  // }
+  m3x3 operator - (const m3x3 other) {
+    return m3x3(this->x[0] - other.x[0], this->y[0] - other.y[0], this->z[0] - other.z[0], this->x[1] - other.x[1], this->y[1] - other.y[1], this->z[1] - other.z[1], this->x[2] - other.x[2], this->y[2] - other.y[2], this->z[2] - other.z[2]);
+  }
   // m2x2 operator * (const m2x2 other) { /* grabs dotProd() */
   //   return m2x2(this->x[0] * other.x[0] + this->y[0] * other.x[1], this->x[0] * other.y[0] + this->y[0] * other.y[1],  this->x[1] * other.x[0] + this->y[1] * other.x[1] , this->x[1] * other.y[0] + this->y[1] * other.y[1]);
   // }
-  // m2x2& operator += (const m2x2& other) { /* maybe loop? */
-  //   this->x[0] += other.x[0]; this->y[0] += other.y[0]; this->x[1] += other.x[1]; this->y[1] += other.y[1]; return *this;
-  // }
-  // m2x2& operator -= (const m2x2& other) { /* maybe loop? */
-  //   this->x[0] -= other.x[0]; this->y[0] -= other.y[0]; this->x[1] -= other.x[1]; this->y[1] -= other.y[1]; return *this;
-  // }
+  m3x3& operator += (const m3x3& other) { /* loop? */
+    this->x[0] += other.x[0]; this->y[0] += other.y[0]; this->z[0] += other.z[0]; this->x[1] += other.x[1]; this->y[1] += other.y[1]; this->z[1] += other.z[1];  return *this;
+  }
+  m3x3& operator -= (const m3x3& other) {
+    this->x[0] += other.x[0]; this->y[0] -= other.y[0]; this->z[0] -= other.z[0]; this->x[1] -= other.x[1]; this->y[1] -= other.y[1]; this->z[1] -= other.z[1];  return *this;
+  }
 };
