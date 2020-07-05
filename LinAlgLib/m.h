066744cd12cb6fdff88c0dd9b4e36d985229e99f
Tknott95 <tknott95@hotmail.com>
2020-07-05 05:25:23 -0600

    m2x2 w/ inits()  => LinAlgLib

---
+++ LinAlgLib/m.h
@@ -0,0 +1,51 @@
+#include<iostream>
+
+/*
+** @AUTHOR     - Trevor Knott
+** @EMAIL      - tk@trevorknott.io
+** @ABSTRACT   - Linear Algebra Library to rid third party libs, 
+*/
+
+template<class T>
+struct m2x2 {
+  // T x1,y1, x2, y2;
+  T x[2],y[2];
+
+  m2x2() : x{0.f, 1.f}, y{0.f, 1.f} {}
+  // x[0(0), x[1](0), y[0](0), y[1](0) {}
+  m2x2(T _x[2], T _y[2]) : x(_x), y(_y) {}
+
+  // T mag() { return sqrt(x*x + y*y); };
+
+  // v2 norm() {
+  //   T r = 1 / mag();
+  //   return v2(x*r, y*r); 
+  // }
+
+  // v2 operator + (const v2 other) {
+  //   return v2(this->x + other.x, this->y + other.y);
+  // }
+  // v2 operator - (const v2 other) {
+  //   return v2(this->x - other.x, this->y - other.y);
+  // }
+  // v2 operator * (const T& other) {
+  //   return v2(this->x * other, this->y * other);
+  // }
+  // v2 operator / (const T& other) {
+  //   return v2(this->x / other, this->y / other);
+  // }
+
+  // v2& operator += (const v2& other) {
+  //   this->x += other.x; this->y += other.y; return *this;
+  // }
+  // v2& operator -= (const v2& other) {
+  //   this->x -= other.x; this->y -= other.y; return *this;
+  // }
+  // v2& operator *= (const v2& other) {
+  //   this->x *= other.x; this->y *= other.y; return *this;
+  // }
+  // v2& operator /= (const v2& other) {
+  //   this->x /= other.x; this->y /= other.y; return *this;
+  // }
+
+};
--- LinAlgLib/main.cpp
+++ LinAlgLib/main.cpp
@@ -1,4 +1,5 @@
 #include "v.h"
+#include "m.h"
 
 int main() {
   v2<float> v00;
@@ -11,5 +12,8 @@ int main() {
   printf("v00(%f, %f)\n", v00.x, v00.y);
   printf("v01(%f, %f)\n", v01.x, v01.y);
 
+  m2x2<float> m00;
+  printf("\nm2x2[[%f, %f], [%f, %f])\n", m00.x[0], m00.y[0], m00.x[1], m00.y[1]);
+  
   return 0;
 }
