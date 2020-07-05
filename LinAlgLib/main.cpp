cae6abbe875764ac7509200823a255f276f8f818
Tknott95 <tknott95@hotmail.com>
2020-07-05 13:22:25 -0600

    DotProd w/ AiExample on Lib working -> LinAlgLib

--- LinAlgLib/main.cpp
+++ LinAlgLib/main.cpp
@@ -34,14 +34,11 @@ int main() {
 
  /* - ##################################### - */
   v4<int> inputs = {1,2,3,4};
-  v4<float> v2_00= {-0.4f, 0.7f, 0.2f, 0.4f};
-  v4<float> v2_01= {1.4, -0.8, -0.4, 0.4};
-
-  v2<v4<float>> weights = {v2_00, v2_01};
-  float biases[] = {0.2, 0.4};
+  v2<v4<float>> weights = {{-0.4f, 0.7f, 0.2f, 0.4f}, {1.4, -0.8, -0.4, 0.4}};
+  v2<float> biases = {0.2, 0.4};
 
   printf("\n i[0]: %i \n i[1]: %i \n i[2]: %i\n i[3]: %i", inputs.val[0], inputs.val[1],  inputs.val[2], inputs.val[3]);
-  v2<float> output = {{inputs.val[0]*weights.val[0].val[0] + inputs.val[1]*weights.val[0].val[1]+ inputs.val[2]*weights.val[0].val[2] + inputs.val[3]*weights.val[0].val[3] + biases[0]}, {inputs.val[0]*weights.val[1].val[0] + inputs.val[1]*weights.val[1].val[1] + inputs.val[2]*weights.val[1].val[2] + inputs.val[3]*weights.val[1].val[3] + biases[1]}};
+  v2<float> output = {{inputs.val[0]*weights.val[0].val[0] + inputs.val[1]*weights.val[0].val[1]+ inputs.val[2]*weights.val[0].val[2] + inputs.val[3]*weights.val[0].val[3] + biases.val[0]}, {inputs.val[0]*weights.val[1].val[0] + inputs.val[1]*weights.val[1].val[1] + inputs.val[2]*weights.val[1].val[2] + inputs.val[3]*weights.val[1].val[3] + biases.val[1]}};
 
   float test = weights.val[0].val[0];
   printf("\n\n weigths[0][0]: %.2f", test);
