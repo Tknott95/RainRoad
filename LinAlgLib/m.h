67385d47d032abe95777f46228ba9823a5dc8b6c
Tknott95 <tknott95@hotmail.com>
2020-07-05 14:05:59 -0600

    Working dotProd w/ check -> LinAlgLib

--- LinAlgLib/m.h
+++ LinAlgLib/m.h
@@ -25,11 +25,9 @@ struct m2x2 {
   m2x2 operator - (const m2x2 other) {
     return m2x2(this->x[0] - other.x[0], this->y[0] - other.y[0], this->x[1] - other.x[1], this->y[1] - other.y[1]);
   }
-
   m2x2 operator * (const m2x2 other) { /* grabs dotProd() */
     return m2x2(this->x[0] * other.x[0] + this->y[0] * other.x[1], this->x[0] * other.y[0] + this->y[0] * other.y[1],  this->x[1] * other.x[0] + this->y[1] * other.x[1] , this->x[1] * other.y[0] + this->y[1] * other.y[1]);
   }
-
   m2x2& operator += (const m2x2& other) { /* maybe loop? */
     this->x[0] += other.x[0]; this->y[0] += other.y[0]; this->x[1] += other.x[1]; this->y[1] += other.y[1]; return *this;
   }
