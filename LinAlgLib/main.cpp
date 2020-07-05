5689fcc094af5a94d731173dae34b71183505b03
Tknott95 <tknott95@hotmail.com>
2020-07-05 13:19:09 -0600

    DotProd test w/ dims -> LinAlgLib

--- LinAlgLib/main.cpp
+++ LinAlgLib/main.cpp
@@ -6,11 +6,11 @@ int main() {
   v2<float> v01 = {1.0f, 1.0f};
 
   printf("v00(%f, %f)\n", v00.x, v00.y);
-  // // v00 = v00 + v01 + v01 + v01;
-  // v00 += v01 += v01 *= v01 /= {1.0, 1.0};
-  // v01 += v01;
-  // printf("v00(%f, %f)\n", v00.x, v00.y);
-  // printf("v01(%f, %f)\n", v01.x, v01.y);
+  v00 = v00 + v01 + v01 + v01;
+  v00 += v01 += v01 *= v01 /= {1.0, 1.0};
+  v01 += v01;
+  printf("v00(%f, %f)\n", v00.x, v00.y);
+  printf("v01(%f, %f)\n", v01.x, v01.y);
 
   m2x2<float> m00;
   float m01_x[2] = {5.f, 7.f};
@@ -22,15 +22,10 @@ int main() {
   m2x2<float> m02 = {m02_x, m02_y};
 
 
-  // printf("\nm00[[%f, %f], [%f, %f])\n", m00.x[0], m00.y[0], m00.x[1], m00.y[1]);
-  // printf("m01[[%f, %f], [%f, %f])\n", m01.x[0], m01.y[0], m01.x[1], m01.y[1]);
-
   printf("\n\n - b4 dotProd() \n");
   printf("m01[\n[%f, %f],\n[%f, %f])\n", m01.x[0], m01.y[0], m01.x[1], m01.y[1]);
   printf("\nm02[\n[%f, %f],\n[%f, %f])\n", m02.x[0], m02.y[0], m02.x[1], m02.y[1]);
-
   m01 = m01 * m02; /* dotProd() order does matter */
-
   printf("\n  - after dotProd()");
   printf("\nm01[\n[%f, %f],\n[%f, %f])\n", m01.x[0], m01.y[0], m01.x[1], m01.y[1]);
 
@@ -48,16 +43,11 @@ int main() {
   printf("\n i[0]: %i \n i[1]: %i \n i[2]: %i\n i[3]: %i", inputs.val[0], inputs.val[1],  inputs.val[2], inputs.val[3]);
   v2<float> output = {{inputs.val[0]*weights.val[0].val[0] + inputs.val[1]*weights.val[0].val[1]+ inputs.val[2]*weights.val[0].val[2] + inputs.val[3]*weights.val[0].val[3] + biases[0]}, {inputs.val[0]*weights.val[1].val[0] + inputs.val[1]*weights.val[1].val[1] + inputs.val[2]*weights.val[1].val[2] + inputs.val[3]*weights.val[1].val[3] + biases[1]}};
 
-//  float output[2][1] = {
-//     {inputs.val[0]*w[0][0] + inputs.val[1]*w[0][1] + inputs.val[2]*w[0][2] + inputs.val[3]*w[0][3] + biases[0]},
-//     {inputs.val[0]*w[1][0] + inputs.val[1]*w[1][1] + inputs.val[2]*w[1][2] + inputs.val[3]*w[1][3] + biases[1]}
-//   };
-
   float test = weights.val[0].val[0];
   printf("\n\n weigths[0][0]: %.2f", test);
   // i = i * w[0];
-  // printf("\n\n output[0][0]: %f", output[0][0]);
-  // printf("\n output[1][0]: %f", output[1][0]);
+  printf("\n\n output[0]: %f", output.val[0]);
+  printf("\n output[1]: %f", output.val[1]);
 
   return 0;
 }
