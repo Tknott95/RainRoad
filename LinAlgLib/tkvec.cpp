75091e0faa0067b766a2ba9e6d4cadda0268a24b
Tknott95 <tknott95@hotmail.com>
2020-07-05 04:46:14 -0600

    Working w/ operator add  => LinAlgLib

--- LinAlgLib/tkvec.cpp
+++ LinAlgLib/tkvec.cpp
@@ -30,6 +30,11 @@ struct v2 {
   v2 operator / (const T& other) {
     return v2(this->x / other, this->y / other);
   }
+
+  v2& operator += (const v2& other) {
+    this->x += other.x; this->y += other.y; return *this;
+  } 
+
 };
 
 
@@ -51,7 +56,9 @@ int main() {
   v2<float> v00;
   v2<float> v01 = {1.0f, 1.0f};
 
-  v00 = v00 + v01 + v01 + v01;
+  printf("v00(%f, %f)\n", v00.x, v00.y);
+  // v00 = v00 + v01 + v01 + v01;
+  v00 += v01 += v01 += v01;
 
   printf("v00(%f, %f)\n", v00.x, v00.y);
   printf("v01(%f, %f)\n", v01.x, v01.y);
