6fb2c650f955c75a662b55c3f0455752620ea03f
Tknott95 <tknott95@hotmail.com>
2020-07-05 21:44:03 -0600

    mDynCout -> LinAlgLib

--- LinAlgLib/m.h
+++ LinAlgLib/m.h
@@ -168,8 +168,8 @@ struct mDyn {
   mDyn(T _val[rowSize][colSize]) : val(_val[rowSize][colSize]) {}
   
   void construct() {
-    for(int i =0; i >= rowSize; i++) {
-      for(int j =0; j >= colSize; i++) { 
+    for(int i =0; i <= rowSize; i++) {
+      for(int j =0; j <= colSize; i++) { 
         this->val[i][j] = i + j % 2; 
       }
     }
