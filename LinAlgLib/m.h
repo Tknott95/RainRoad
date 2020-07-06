c8f10031e3ac3647f303cbe5d4d62b54a855072e
Tknott95 <tknott95@hotmail.com>
2020-07-05 19:54:52 -0600

    m3x3 fixed -> LinAlgLib

--- LinAlgLib/m.h
+++ LinAlgLib/m.h
@@ -38,36 +38,47 @@ struct m2x2 {
 
 template<class T>
 struct m3x3 {
-  T val[9];
+  T val[3][3];
   std::map<std::string, T> getVal;
 
   m3x3() : val{0, 0, 0, 1, 1, 1, 2, 2, 2} {}
   m3x3(T _x1, T _y1, T _z1, T _x2, T _y2, T _z2, T _x3, T _y3, T _z3) : val{_x1, _y1, _z1, _x2, _y2, _z2, _x3, _y3, _z3} {
+    // getVal = { 
+    //   {"x0", val[0]},
+    //   {"y0", val[1]},
+    //   {"z0", val[2]},
+    //   {"x1", val[3]},
+    //   {"y1", val[4]},
+    //   {"z1", val[5]},
+    //   {"x2", val[6]},
+    //   {"y2", val[7]},
+    //   {"z2", val[8]}
+    // };
     getVal = { 
-      {"x0", val[0]},
-      {"y0", val[1]},
-      {"z0", val[2]},
-      {"x1", val[3]},
-      {"y1", val[4]},
-      {"z1", val[5]},
-      {"x2", val[6]},
-      {"y2", val[7]},
-      {"z2", val[8]}
+      {"x0", val[0][0]},
+      {"y0", val[0][1]},
+      {"z0", val[0][2]},
+      {"x1", val[1][0]},
+      {"y1", val[1][1]},
+      {"z1", val[1][2]},
+      {"x2", val[2][0]},
+      {"y2", val[2][1]},
+      {"z2", val[2][2]}
     };
   }
 
   m3x3 operator + (const m3x3 other) {
-    return m3x3(this->getVal.at("x0") + other.getVal.at("x0"), this->getVal.at("y0") + other.getVal.at("y0"), this->getVal.at("z0") + other.getVal.at("z0"), this->val[3] + other.val[3], this->val[4] + other.val[4], this->val[5] + other.val[5], this->val[6] + other.val[6], this->val[7] + other.val[7], this->val[8] + other.val[8]);
+    return m3x3(this->getVal.at("x0") + other.getVal.at("x0"), this->getVal.at("y0") + other.getVal.at("y0"), this->getVal.at("z0") + other.getVal.at("z0"), this->getVal.at("x1") + other.getVal.at("x1"), this->val[1][1] + other.val[1][1], this->val[1][2] + other.val[1][2], this->val[2][0] + other.val[2][0], this->val[2][1] + other.val[2][1], this->val[2][2] + other.val[2][2]);
   }
   m3x3 operator - (const m3x3 other) {
-    return m3x3(this->val[0] - other.val[0], this->val[1] - other.val[1], this->val[2] - other.val[2], this->val[3] + other.val[3], this->val[4] + other.val[4], this->val[5] + other.val[5], this->val[6] + other.val[6], this->val[7] + other.val[7], this->val[8] + other.val[8]);
+    return m3x3(this->val[0][0] - other.val[0][0], this->val[0][1] - other.val[0][1], this->val[0][2] - other.val[0][2], this->val[1][0] - other.val[1][0], this->val[1][1] - other.val[1][1], this->val[1][2] - other.val[1][2], this->val[2][0] - other.val[2][0], this->val[2][1] - other.val[2][1], this->val[2][2] - other.val[2][2]);
   }
 
   m3x3& operator += (const m3x3& other) { /* loop? */
-    this->val[0] += other.val[0]; this->val[1] += other.val[1]; this->val[2] += other.val[2]; this->val[3] += other.val[3]; this->val[4] += other.val[4]; this->val[5] += other.val[5]; this->val[6] += other.val[6]; this->val[7] += other.val[7]; this->val[8] += other.val[8];  return *this;
+    this->val[0][0] += other.val[0][0]; this->val[0][1] += other.val[0][1]; this->val[0][2] += other.val[0][2]; this->val[1][0] += other.val[1][0]; this->val[1][1] += other.val[1][1]; this->val[1][2] += other.val[1][2]; this->val[2][0] += other.val[2][0]; this->val[2][1] += other.val[2][1]; this->val[2][2] += other.val[2][2];  return *this;
   }
   m3x3& operator -= (const m3x3& other) {
-    this->val[0] -= other.val[0]; this->val[1] -= other.val[1]; this->val[2] -= other.val[2]; this->val[3] -= other.val[3]; this->val[4] -= other.val[4]; this->val[5] -= other.val[5]; this->val[6] -= other.val[6]; this->val[7] -= other.val[7]; this->val[8] -= other.val[8];  return *this;
+    this->val[0][0] -= other.val[0][0]; this->val[0][1] -= other.val[0][1]; this->val[0][2] -= other.val[0][2]; this->val[1][0] -= other.val[1][0]; this->val[1][1] -= other.val[1][1]; this->val[1][2] -= other.val[1][2]; this->val[2][0] -= other.val[2][0]; this->val[2][1] -= other.val[2][1]; this->val[2][2] -= other.val[2][2];  return *this;
   }
 };
 
