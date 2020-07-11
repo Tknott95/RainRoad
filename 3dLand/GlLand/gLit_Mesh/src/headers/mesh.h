885110c4f287b389fbc054ba155a39778a66a0ed
Trevor Knott <Tknott95@hotmail.com>
2020-07-11 14:44:51 -0600

    MeshClass genShader prep 0.0.1  -> gLit_Mesh

--- 3dLand/GlLand/gLit_Mesh/src/headers/mesh.h
+++ 3dLand/GlLand/gLit_Mesh/src/headers/mesh.h
@@ -3,23 +3,18 @@
 
 #include <string>
 #include <vector>
-#include <shader.h>
+using namespace std;
 
-#include <glad/glad.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+// #include <glad/glad.h>
+// #include <glm/glm.hpp>
+// #include <glm/gtc/matrix_transform.hpp>
 
+#include "shader.h"
 #include "objLoader.h"
 #include "structs/encodedObj.h"
 
-using namespace std;
-/*
-  struct EncodedObj {
-    std::vector<glm::vec3> vertices;
-    std::vector<glm::vec2> uvs;
-    std::vector<glm::vec3> norms;
-  };
-*/
+
+
 class Mesh {
   private:
     EncodedObj encodedObj;
