36658118c0b4327500e9f45d0a6c312458e79a54
Trevor Knott <Tknott95@hotmail.com>
2020-07-11 13:04:29 -0600

    shell script tweaks -> LinAlgLib

--- LinAlgLib/zrv.sh
+++ LinAlgLib/zrv.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+WIN_BINARY=main.exe
+LINUX_BINARY=main
+
+echo "   Cleaning old Binaries"
+if test -f "main.exe"; then
+  rm -f ./main.exe
+elif test -f "main"; then
+  rm -f ./main
+fi
 
 echo "   Compiling and running binary"
 g++ main.cpp -o main && ./main
