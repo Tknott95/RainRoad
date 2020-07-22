50a478bca381f824e95385c1c1ebfe6b251ddf98
Trevor Knott <Tknott95@hotmail.com>
2020-07-22 01:10:33 +0000

    Mesh WORKING v6.0.0 prepping dynamic vecs  -> gl_Mesh

--- 3dLand/GlLand/gLit_Mesh/src/ctrls/draw.cpp
+++ 3dLand/GlLand/gLit_Mesh/src/ctrls/draw.cpp
@@ -1,12 +1,7 @@
 #include "../headers/draw.h"
 
-// void Draw::addToMesh(Mesh _m) {
-//   this->mesh.push_back(_m);
-// };
-
 Draw::Draw()  {  /* onInit() */
   this->mesh.push_back(m00);
-  // this->addToMesh(m00);
 };
 
 Draw::~Draw() { /* deallocate anythin needed here */ };
@@ -18,7 +13,8 @@ void Draw::update(Camera* camera, ivec2 screenSize) {
   // };
 
   this->mesh[0].draw(camera, screenSize);
-  
+  // this->mesh[1].draw(camera, screenSize);
+
  /************* OBJ DRAWING FINISHED **********************/
  /************* SKYBOX DRAWING START **********************/
   this->skybox.draw(camera, screenSize);
