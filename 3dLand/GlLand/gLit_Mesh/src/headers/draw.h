2169a2a09e6d0bdd9d2d5a93fb147925735c6043
Trevor Knott <Tknott95@hotmail.com>
2020-07-22 05:20:04 +0000

    Position 2 conosle log(), etc.   v10.0.0  -> gl_Mesh

--- 3dLand/GlLand/gLit_Mesh/src/headers/draw.h
+++ 3dLand/GlLand/gLit_Mesh/src/headers/draw.h
@@ -8,14 +8,13 @@ using namespace glm;
 
 class Draw {
   private:
-    int meshSize = 5; /* if adding via. very abstract scenrario call meshSize++ for dyn twerkingggg */
     Mesh plane{false, vec3(0.0f,-1.0f, 0.0f), "assets/objects/plane_for_walls.obj"};
     Mesh walls{false, vec3(0.0f,-1.0f, 0.0f), "assets/objects/plane_walls.obj"};
     Mesh m01{false, vec3(2.0f,0.0f,1.0f), "assets/objects/sqr.obj"};
     Mesh m02{false, vec3(2.0f,1.0f,4.0f), "assets/objects/col_lg.obj"};
     Mesh m03{false, vec3(2.0f,1.0f,-4.0f), "assets/objects/col_lg.obj"};
     Mesh skybox{true};
-    Mesh mesh[meshSize] = { /* @TODO make sizing dynamic */
+    Mesh mesh[5] = { /* @TODO make sizing dynamic */
       plane, walls,
       m01,m02,m03
     };
