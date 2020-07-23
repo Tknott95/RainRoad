df37ee80bcf8b47b83e989c94101caf24384c0c7
Trevor Knott <Tknott95@hotmail.com>
2020-07-23 09:31:38 +0000

    HUGE CAMERA twweaks v1.2.0 -> gLit_UI

--- 3dLand/GlLand/gLit_UI/src/headers/camera.h
+++ 3dLand/GlLand/gLit_UI/src/headers/camera.h
@@ -13,26 +13,6 @@ enum CamMovement {
   RIGHT
 };
 
-  // const float YAW=-90.f,
-  //   Pitch=0.f,
-  //   Speed=2.5f,
-  //   Sensitivity=0.1f,
-  //   Zoom=45.f;
-
-
-// struct CamTrans {
-//   glm::vec3 Position;
-//   glm::vec3 Front;
-//   glm::vec3 Up;
-//   glm::vec3 Right;
-//   glm::vec3 WorldUp;
-// };
-
-// const float YAW = -90.0f;
-// const float PITCH = 0.0f;
-// const float SPEED = 2.5f;
-// const float SENSITIVITY = 0.1f;
-// const float ZOOM = 45.0f;
 using namespace glm;
 
 class Camera {
