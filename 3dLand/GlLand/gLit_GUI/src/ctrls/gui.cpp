df1e4ab6291ba98027ea4843ed22c6aac228ba2a
Trevor Knott <Tknott95@hotmail.com>
2020-07-24 06:33:07 +0000

    Chars w/ Insert  v2.1.1 -> gLit_GUI

--- 3dLand/GlLand/gLit_GUI/src/ctrls/gui.cpp
+++ 3dLand/GlLand/gLit_GUI/src/ctrls/gui.cpp
@@ -6,5 +6,38 @@ GUI::GUI(const char* _fontPath, const int _fontSize) {
 
   FT_Set_Pixel_Sizes(ftFace, 0, _fontSize);
 
-  if (FT_Load_Char(face, 'T', FT_LOAD_RENDER) printf("\n\e[0;31;40m GLYPH LOADING ERROR \e[0m");
+  if (FT_Load_Char(ftFace, 'T', FT_LOAD_RENDER)) printf("\n\e[0;31;40m GLYPH LOADING ERROR \e[0m");
+
+  for(unsigned char k=0;k<128;k++) {
+    if(FT_Load_Char(ftFace, k, FT_LOAD_RENDER)) printf("\n\e[0;31;40m GLYPH LOADING ERROR IN LOOP \e[0m");
+
+    uint texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(
+      GL_TEXTURE_2D,                // target
+      0,                            // level
+      GL_RED,                       // internalFormat
+      ftFace->glyph->bitmap.width,  // width
+      ftFace->glyph->bitmap.rows,   // height
+      0,                            // border
+      GL_RED,                       // format
+      GL_UNSIGNED_BYTE,             // type
+      ftFace->glyph->bitmap.buffer  // data
+    );
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    Char thisChar = {
+      texture,
+      glm::ivec2(),
+      glm::ivec2(),
+      ftFace->glyph->advance.x
+    };
+
+    characters.insert(std::pair<char, Char>(k, thisChar));
+  }
 };
--- 3dLand/GlLand/gLit_GUI/src/headers/gui.h
+++ 3dLand/GlLand/gLit_GUI/src/headers/gui.h
@@ -3,17 +3,24 @@
 #include FT_FREETYPE_H
 #include FT_OUTLINE_H
 
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <map>
+
 struct Char {
   uint ID;            // TextureID
   glm::ivec2 Size;    // GlyphSize
   glm::ivec2 Bearing; // Offset from baseline from L to R 
-  uint Advance;       // Offset to advance to next glyph
+  long Advance;       // Offset to advance to next glyph
 };
 
 class GUI {
   private:
     FT_Library  ftLib;
     FT_Face     ftFace;
+
+    std::map<char, Char> characters;
+
   public:
     GUI(const char*, const int _fontSize=12);
 };
