490296e873ba473d65403e75b404fa8e3ddfded8
Trevor Knott <Tknott95@hotmail.com>
2020-07-25 02:41:05 +0000

    brkn: test  v3.0.1 -> gLit_GUI

--- 3dLand/GlLand/gLit_GUI/assets/shaders/font/font.fs
+++ 3dLand/GlLand/gLit_GUI/assets/shaders/font/font.fs
@@ -7,6 +7,6 @@ uniform sampler2D text;
 uniform vec3 textColor;
 
 void main() {
-  vec4 sampled = vec4(1.0, 1.0, 1.0. texture(text, oTexCoords).r);
+  vec4 sampled = vec4(1.0, 1.0, 1.0, texture(text, oTexCoords).r);
   outColor = vec4(textColor, 1.0) * sampled;
 }
