1584c6fb21bc1c43e7c1d72291ab2e14c7c3218c
Trevor Knott <Tknott95@hotmail.com>
2020-07-25 03:18:44 +0000

    brkn: test  v3.0.1 -> gLit_GUI

--- 3dLand/GlLand/gLit_GUI/src/ctrls/draw.cpp
+++ 3dLand/GlLand/gLit_GUI/src/ctrls/draw.cpp
@@ -10,7 +10,7 @@ void Draw::update(Camera* camera, ivec2 screenSize) {
   font.Draw(
     "das one lit gui homie", // TextToRender
     fontShader,              // Shader
-    vec3(5.f, 5.f, 5.f),   // Pos
+    vec3(25.f, 25.f, 1.f),   // (Pos(x,y), and Scale)
     vec3(0.2, 0.2f, 0.6f)    // Color
   );
  /************* OBJ DRAWING START **********************/
