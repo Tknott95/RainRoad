2ff53a3b5aa84040527f9b099b5610b198b42155
Trevor Knott <Tknott95@hotmail.com>
2020-07-25 02:14:25 +0000

    Font::Draw() DONE v2.0.2 -> gLit_GUI

--- 3dLand/GlLand/gLit_GUI/src/ctrls/font.cpp
+++ 3dLand/GlLand/gLit_GUI/src/ctrls/font.cpp
@@ -64,7 +64,7 @@ Font::~Font() {
   glDeleteVertexArrays(0, &myVAO);
 };
 
-void Font::Draw(string _text ,Shader &_shader, vec3 _posAndScale, vec3 _color) {
+void Font::Draw(const char* _text ,Shader &_shader, vec3 _posAndScale, vec3 _color) {
   _shader.use();
   glUniform3f(glGetUniformLocation(_shader.ID, "textColor"), _color.x, _color.y, _color.z);
   glActiveTexture(GL_TEXTURE0);
@@ -75,7 +75,7 @@ void Font::Draw(string _text ,Shader &_shader, vec3 _posAndScale, vec3 _color) {
     Char myChar = characters[*stringChar];
 
     float xPos = _posAndScale.x + myChar.Bearing.x * _posAndScale[3];
-    float yPos = _posAndScale.y - (myChar.Size.y - myChar.Bearing,y) * _posAndScale[3];
+    float yPos = _posAndScale.y - (myChar.Size.y - myChar.Bearing.y) * _posAndScale[3];
     float width = myChar.Size.x * _posAndScale[3];
     float height = myChar.Size.y * _posAndScale[3];
 
