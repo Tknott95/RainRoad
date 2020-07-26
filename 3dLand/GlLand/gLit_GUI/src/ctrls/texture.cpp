45d08800a5a6dbd7e327e7185aca8ca38ea925ef
Trevor Knott <Tknott95@hotmail.com>
2020-07-26 02:11:00 +0000

    Texture dynamic GlEnum, etc tweaks v4.0.0 -> gLit_GUI

--- 3dLand/GlLand/gLit_GUI/src/ctrls/texture.cpp
+++ 3dLand/GlLand/gLit_GUI/src/ctrls/texture.cpp
@@ -1,6 +1,6 @@
 #include "../headers/texture.h"
 
-uint Texture::load(const char* path) {
+uint Texture::load(const char* path, GLenum _textureClamping) {
   uint texID;
   glGenTextures(1, &texID);
 
@@ -16,8 +16,9 @@ uint Texture::load(const char* path) {
     glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
     glGenerateMipmap(GL_TEXTURE_2D);
 
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    /* GL_REPEAT GL_CLAMP_TO_EDGE GL_CLAMP_TO_BORDER */
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, _textureClamping);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, _textureClamping);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
--- 3dLand/GlLand/gLit_GUI/src/headers/texture.h
+++ 3dLand/GlLand/gLit_GUI/src/headers/texture.h
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <map>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "third_party/stb_image.h"
@@ -7,6 +9,6 @@
 
 class Texture {
   public:
-    uint load(const char* path);
+    uint load(const char* path, GLenum _textureClamping=GL_CLAMP_TO_EDGE);
     uint loadSkybox(std::vector<std::string>);
 };
