bb73ba7902861a8656b1afdc1d661f2b2c04f637
Trevor Knott <Tknott95@hotmail.com>
2020-08-05 04:39:39 +0000

    v1.2.0 AABB PREP | naming methods |  - gLit_AABB

--- 3dLand/GlLand/gLit_AABB/src/headers/draw.h
+++ 3dLand/GlLand/gLit_AABB/src/headers/draw.h
@@ -38,6 +38,8 @@ class Draw {
 
     void renderGUI(Camera* camera, ivec2 screenSize, int fps);
 
+    void isPointInsideAABB(vec3 myPos, float[6] other);
+    void isAABBInsideAABB(float[6] this, float[6] other);
   public:
     Draw();
     virtual ~Draw();
