eae1f8b6a80691df82f2f7bc9bad6b2dea5c4927
Trevor Knott <Tknott95@hotmail.com>
2020-08-06 13:09:40 +0000

    Collision w/ boolsForSides .cpp|.h  v1.0.0 -> gLit_AABB

--- 3dLand/GlLand/gLit_AABB/src/ctrls/collision.cpp
+++ 3dLand/GlLand/gLit_AABB/src/ctrls/collision.cpp
@@ -10,15 +10,21 @@ bool Collision::PointInsideAABB(vec3 _pos, vec3 _otherPos) {
   bool y1  = _pos.y <= _otherPos.y + _otherMag.y;
   bool z0  = _pos.z >= _otherPos.z - _otherMag.z;
   bool z1  = _pos.z <= _otherPos.z + _otherMag.z;
+  bool hasCollided = (x0 && x1) && (y0 && y1) && (z0 && z1);
 
-  bool returnBool = (x0 && x1) && (y0 && y1) && (z0 && z1);
+  /* TRACKING FACE COLLIDED WITH */
+  if(x0) printf("\e[2;34;40m x0 HIT \e[0m\n");
+  if(x1) printf("\e[2;34;40m x1 HIT \e[0m\n");
+  if(y0) printf("\e[2;34;40m y0 HIT \e[0m\n");
+  if(y1) printf("\e[2;34;40m y1 HIT \e[0m\n");
+  if(z0) printf("\e[2;34;40m z0 HIT \e[0m\n");
+  if(z1) printf("\e[2;34;40m z1 HIT \e[0m\n");
 
-  if(returnBool) {
+  if(hasCollided) /* printf("\e[2;33;40m AABB(%f, %f, %f)\e[0m\n", PosAfterCol.x, PosAfterCol.y, PosAfterCol.z); */
     this->PosAfterCol = this->PosBeforeCol;
-    printf("\e[2;33;40m AABB(%f, %f, %f)\e[0m\n", PosAfterCol.x, PosAfterCol.y, PosAfterCol.z);
-  };
 
-  return returnBool;
+
+  return hasCollided;
 };
 
 bool Collision::AABBInsideAABB(vec3 _pos, vec3 _otherPos) {
