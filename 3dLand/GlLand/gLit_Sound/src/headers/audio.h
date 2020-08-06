caf8ba1ea1e5702370d4834c5c8f3ffc7e524470
Trevor Knott <Tknott95@hotmail.com>
2020-08-06 15:53:54 +0000

    OpenAL for sound prep  v0.0.1 -> gLit_Sound

--- 3dLand/GlLand/gLit_Sound/src/headers/audio.h
+++ 3dLand/GlLand/gLit_Sound/src/headers/audio.h
@@ -1,6 +1,12 @@
+#include <AL/al.h>
+#include <AL/alc.h>
 
+/* @TODO make all public names Uppercase and not CamelCase */
 class Audio {
   private:
+    Audio();
+    virtual ~Audio();
 
   public:
+    void Update();
 };
