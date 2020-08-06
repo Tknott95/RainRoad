df6da3afe83cc729354cc1e4860601a84f07eced
Trevor Knott <Tknott95@hotmail.com>
2020-08-06 17:37:23 +0000

    OpenAL for sound prep  v0.1.0 -> gLit_Sound

--- 3dLand/GlLand/gLit_Sound/src/ctrls/audio.cpp
+++ 3dLand/GlLand/gLit_Sound/src/ctrls/audio.cpp
@@ -4,8 +4,7 @@ void Audio::loadWavFile(const char* _path, Wav _wav) {
   /* Will use Third Party Lib until M.V.P then will refactor */
   char buffer[4];
 
-  ifstream _file;
-  _file.open(_path);
+  ifstream _file.open(_path);
 
   if(!_file.read(buffer, 4)) printf("\n\e[0;31;40m Wav -> RIFF read ERROR \e[0m");
   if(/*std::*/strncmp(buffer, "RIFF", 4) != 0) printf("\n\e[0;31;40m Wav -> not valid .wav\n   HEADER DOESN'T BEGIN W/ RIFF \e[0m");
