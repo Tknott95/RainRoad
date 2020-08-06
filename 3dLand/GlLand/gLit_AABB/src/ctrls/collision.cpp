b7f17dc68efd68044238fabc7acc224981433c03
Trevor Knott <Tknott95@hotmail.com>
2020-08-06 13:33:16 +0000

    Tracking sides  v1.1.1 -> gLit_AABB

--- 3dLand/GlLand/gLit_AABB/src/ctrls/collision.cpp
+++ 3dLand/GlLand/gLit_AABB/src/ctrls/collision.cpp
@@ -3,13 +3,13 @@
 bool Collision::PointInsideAABB(vec3 _pos, vec3 _otherPos) {
   const vec3 _otherMag{1.25f}; /* @TODO make this dynamic */
 
-  /* Modularized for tracking "the side collided" */
   bool x0  = _pos.x >= _otherPos.x - _otherMag.x;
   bool x1  = _pos.x <= _otherPos.x + _otherMag.x;
   bool y0  = _pos.y >= _otherPos.y - _otherMag.y;
   bool y1  = _pos.y <= _otherPos.y + _otherMag.y;
   bool z0  = _pos.z >= _otherPos.z - _otherMag.z;
   bool z1  = _pos.z <= _otherPos.z + _otherMag.z;
+
   bool hasCollided = (x0 && x1) && (y0 && y1) && (z0 && z1);
   /********************
   * x1 -> FRONT
@@ -23,8 +23,7 @@ bool Collision::PointInsideAABB(vec3 _pos, vec3 _otherPos) {
   *  1)  if(y1) printf("\e[2;1;40m TOP HIT \e[0m");
   *  2)  printf("\e[2;33;40m AABB(%f, %f, %f)\e[0m\n", PosAfterCol.x, PosAfterCol.y, PosAfterCol.z);
   ********************/
-  if(hasCollided) /*  */
-    this->PosAfterCol = this->PosBeforeCol;
+  if(hasCollided) this->PosAfterCol = this->PosBeforeCol;
 
   return hasCollided;
 };
