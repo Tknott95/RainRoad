f75d826b518643c243182f3fb15effa04e5d6eef
Trevor Knott <Tknott95@hotmail.com>
2020-08-06 16:43:01 +0000

    OpenAL for sound prep  v0.1.0 -> gLit_Sound

--- 3dLand/GlLand/gLit_Sound/src/ctrls/audio.cpp
+++ 3dLand/GlLand/gLit_Sound/src/ctrls/audio.cpp
@@ -3,3 +3,11 @@
 Audio::Audio() {
   printf(" Audio Initialized...");
 };
+
+Audio::~Audio() {
+  /* Delete all audio buffers here */
+};
+
+void Audio::Update() {
+  /* FixedUpdate loop also? Better cals running both */
+};
