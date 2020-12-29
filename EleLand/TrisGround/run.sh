a3dec701ac1730935683379b2654e1affb0b5cb0
Trevor Knott <Tknott95@hotmail.com>
2020-12-28 19:31:10 -0700

    Checking for old deletions and fixing some scripts | TrisGround

--- EleLand/TrisGround/run.sh
+++ EleLand/TrisGround/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Clean main or create a .git. I Prefer to clean main here. 
-g++ -Wall main.cpp -lglfw -lGL -o bin/main && ./bin/main
+g++ -Wall main.cpp -lglfw -lGL -o runtime/main && ./runtime/main
 # g++ -Wall main.cpp -lglfw3 -lopengl32 -lgdi32 -luser32 -lkernel32 -lglfw -lGL -lglfw3dll -lopengl32 -o main && ./main.exe
