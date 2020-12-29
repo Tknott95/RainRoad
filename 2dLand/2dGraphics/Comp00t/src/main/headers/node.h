802d0b3a138310905a2a407ecca2037477a4c4e2
Trevor Knott <Tknott95@hotmail.com>
2020-12-28 19:23:38 -0700

    Comp00t back in after july 5th fkup | Comp00t

---
+++ 2dLand/2dGraphics/Comp00t/src/main/headers/node.h
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <iostream>
+
+using namespace std;
+
+class Node {
+  private:
+    const int totalNodes = 1844; /* @TODO | MAKE DYNAMIC w/ vertices[<#>] VARIABLE */
+    const float spawnDelay = 0.1f;
+    sf::CircleShape _node;
+    vector<sf::CircleShape> _nodes;
+    sf::Vertex vertices[2000]; /* @TODO | MAKE DYNAMIC w/ TOTALNODES VARIABLE */
+    sf::Font _font;
+    sf::Text _text;
+    // sf::VertexArray lines(sf::PrimitiveType); // THIS IS REPLACED BY DYNAMIC sf::Vertex for meow meow
+    // LineStrip // vertexCount Initial number of vertices in the array
+
+    void init();
+    void spawn(float, float);
+
+  public:
+    Node();
+    virtual ~Node();
+    void update();
+    void render(sf::RenderTarget& target);
+};
